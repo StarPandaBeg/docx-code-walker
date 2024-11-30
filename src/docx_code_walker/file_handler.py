42c33224ea4d598c61e220ebb5b98d1d5c7e51b8
StarPandaBeg <kshmurakov@mail.ru>
2024-11-30 20:06:15 +0300

    feat: add ability to ignore files

--- src/docx_code_walker/file_handler.py
+++ src/docx_code_walker/file_handler.py
@@ -1,10 +1,12 @@
 import os
+import ignorelib
 
 def iterate_files(entry: str, keep_folder_name: bool = False, only_name: bool = False):
   if os.path.isfile(entry):
     yield _to_file_entry(entry, os.path.dirname(entry), only_name)
     return
-  for dpath, _, fnames in os.walk(entry):
+  filter = _setup_ignorefilter(entry)
+  for dpath, _, fnames in filter.walk():
     for fname in fnames:
       path = os.path.join(dpath, fname)
       yield _to_file_entry(path, entry, keep_folder_name, only_name)
@@ -20,3 +22,16 @@ def _to_file_entry(entry: str, path: str, keep_folder_name: bool = False, only_n
   if only_name:
     relpath = os.path.basename(relpath)
   return (realpath, relpath)
+
+def _setup_ignorefilter(path):
+  return ignorelib.IgnoreFilterManager(path, 
+    ignore_file_name=".gitignore",
+    global_filters=[
+      ignorelib.IgnoreFilter(
+        patterns=[
+          ".git",
+          ".gitignore",
+        ]
+      )
+    ]
+  )
