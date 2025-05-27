64fff76cf67f5cd68f0ffb91f2dcb012f1bb569b
StarPandaBeg <kshmurakov@mail.ru>
2025-05-27 22:50:40 +0300

    feat: add encoding option

--- src/docx_code_walker/cli.py
+++ src/docx_code_walker/cli.py
@@ -14,9 +14,10 @@ CONTEXT_SETTINGS = dict(
 @click.option("-p", "--preserve-directory", type=bool, is_flag=True, help="Сохранять имя папки при выводе пути к файлу")
 @click.option("-n", "--only-name", type=bool, is_flag=True, help="Сохранять только имя файла при выводе пути")
 @click.option("-c", "--columns", default=2, show_default=True, help="Количество столбцов в итоговом документе")
+@click.option("-e", "--encoding", default='utf-8', show_default=True, help="Кодировка файлов")
 @click.option("-o", "--output", type=click.Path(file_okay=True, writable=True), default="output.docx", show_default=True, help="Итоговый файл")
 @click.option("--font", type=(str, int), default=("Consolas", 7), show_default=True, help="Шрифт")
-def cli(path: click.Path, preserve_directory: bool, only_name: bool, columns: int, output: click.Path, font: tuple[str, int]):
+def cli(path: click.Path, preserve_directory: bool, only_name: bool, columns: int, encoding: str, output: click.Path, font: tuple[str, int]):
   """Простая программа для формирования docx документа из списка файлов
   
   Пригодится для оформления листинга кода в лабораторных, курсовых работах, дипломах и т.д.
@@ -27,13 +28,13 @@ def cli(path: click.Path, preserve_directory: bool, only_name: bool, columns: in
   
   document = WordDocument(columns, font)
   for entry in path:
-    process_entry(document, entry, preserve_directory, only_name)
+    process_entry(document, entry, preserve_directory, only_name, encoding)
   document.save(output)
 
-def process_entry(document: WordDocument, entry: str, preserve_directory: bool, only_name: bool):
+def process_entry(document: WordDocument, entry: str, preserve_directory: bool, only_name: bool, encoding: str):
   for path, name in fh.iterate_files(entry, preserve_directory, only_name):
     print(f"Обработка файла: {click.format_filename(name)}")
-    with click.open_file(path, 'r') as f:
+    with click.open_file(path, 'r', encoding=encoding) as f:
       content = f.read().strip()
       if len(content) == 0:
         continue
